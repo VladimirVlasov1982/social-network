2535f7406fd194972d0f730b2c52c8d8f23d7983
VladimirVlasov1982 <vladimir.vlasov82@mail.ru>
2022-07-06 15:43:20 +0300

    Done

--- api_posts_test.py
+++ api_posts_test.py
@@ -1,27 +1,14 @@
 import pytest
-from utils import Posts
 from app import app
 
+posts_key_should_be = {"poster_name", "poster_avatar", "pic", "content", "views_count", "likes_count", "pk"}
 
-@pytest.fixture()
-def post():
-    post_instance = Posts()
-    return post_instance
+def test_api_posts():
+    response = app.test_client().get('/api/posts')
+    assert type(response.json) == list
+    assert response.json[0].keys() == posts_key_should_be
 
-key_should_be = {"poster_name", "poster_avatar", "pic", "content", "views_count", "likes_count", "pk"}
-
-
-class TestApiPosts:
-
-    def test_api_posts(self):
-        """Тестируем эндпоинт GET /api/posts. Проверяет возвращение списка и соответствие ключей."""
-        response = app.test_client().get('/api/posts')
-        assert type(response.json) == list, "Возвращается не список"
-        assert response.json[0].keys() == key_should_be, "Неверный список ключей"
-
-    def test_api_post_by_id(self,post):
-        """Тестируем эндпоинт GET /api/posts/<post_id>. Проверяет возвращение словаря и соответствие ключей."""
-        response = app.test_client().get('/api/posts/1')
-        assert type(response.json) == dict, "Возвращает не словарь"
-        assert response.json.keys() == key_should_be, "Неверный список ключей"
-
+def test_api_post_by_id():
+    response = app.test_client().get('/api/posts/1')
+    assert type(response.json) == dict
+    assert response.json.keys() == posts_key_should_be
--- utils.py
+++ utils.py
@@ -1,17 +1,20 @@
 import json
+from json import JSONDecodeError
 
 
 class Posts:
+    def __init__(self, path):
+        self.path = path
 
     def get_all_posts(self):
         """Загружаем посты из json."""
-        with open("data/data.json", "r", encoding="utf-8") as file:
-            return json.load(file)
-
-    def get_all_comments(self):
-        """Загружаем комментарии из json."""
-        with open('data/comments.json', "r", encoding="utf-8") as file:
-            return json.load(file)
+        try:
+            with open(self.path, "r", encoding="utf-8") as file:
+                return json.load(file)
+        except FileNotFoundError:
+            return "Файл не найден"
+        except JSONDecodeError:
+            return "Файл не удается пеобразовать"
 
     def get_posts_by_user(self, username):
         """Получаем посты по имени пользователя."""
@@ -20,7 +23,7 @@ class Posts:
         if posts_by_user:
             return posts_by_user
         else:
-            raise ValueError("У пользователя не постов")
+            raise ValueError("У пользователя нет постов")
 
     def get_post_by_id(self, post_id):
         """Получаем посты по id."""
@@ -28,6 +31,49 @@ class Posts:
         for post in posts:
             if post["pk"] == post_id:
                 return post
+
+    def search_posts(self, search):
+        """Поиск постов по содержимому."""
+        posts = self.get_all_posts()
+        search_lower = search.lower()
+        characters = '#,.!:'
+        search_posts = []
+        for post in posts:
+            for sign in characters:
+                new_str = post['content'].lower().replace(sign, "").split()
+                for word in new_str:
+                    if search_lower == word and post not in search_posts:
+                        search_posts.append(post)
+        return search_posts
+
+    def get_posts_by_tagname(self, tagname):
+        """Поиск постов с тегами."""
+        tag_posts = []
+        tagname = '#' + tagname
+        posts = self.get_all_posts()
+        for post in posts:
+            if tagname in post['content'].split():
+                tag_posts.append(post)
+        return tag_posts
+
+    def get_tag_from_post(self, post):
+        """Получение тэга из поста"""
+        tag = []
+        for word in post['content'].split():
+            if word[:1] == '#':
+                tag.append(word)
+        return tag
+
+
+class Comments:
+
+    def __init__(self, path):
+        self.path = path
+
+    def get_all_comments(self):
+        """Загружаем комментарии из json."""
+        with open(self.path, "r", encoding="utf-8") as file:
+            return json.load(file)
 
     def get_comments_by_post_id(self, post_id):
         """Получаем комментарии по id поста."""
@@ -38,30 +84,34 @@ class Posts:
         else:
             raise ValueError("Комментариев нет")
 
-    def search_posts(self, search):
-        """Поиск постов по содержимому."""
-        posts = self.get_all_posts()
-        search_lower = search.lower()
-        other_characters = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
-        search_posts = []
-        for post in posts:
-            for sign in other_characters:
-                if sign in post['content']:
-                    post['content'] = post['content'].replace(sign, "")
-            if search_lower in post['content'].lower().split():
-                search_posts.append(post)
-        return search_posts
 
-    def search_tag_post(self):
-        """Поиск постов с тегами."""
-        tag_posts = []
-        posts = self.get_all_posts()
-        for post in posts:
-            if '#' in post['content']:
-                tag_posts.append(post)
-        return tag_posts
+class Bookmarks:
 
-    def add_to_bookmarks(self, post):
-        """Добавление поста в закладки"""
-        with open('data/bookmarks.json', "w", encoding='utf-8') as file:
-            json.dump(post, file, ensure_ascii=False)
+    def __init__(self, path):
+        self.path = path
+
+    def get_bookmarks(self):
+        """Загрузка данных из закладок"""
+        with open(self.path, 'r', encoding='utf-8') as file:
+            bookmarks = json.load(file)
+            return bookmarks
+
+    def save_bookmarks(self, bookmarks):
+        # Сохраняем закладки
+        with open(self.path, "w", encoding="utf-8") as file:
+            json.dump(bookmarks, file, ensure_ascii=False, indent=4)
+
+    def add_bookmarks(self, post):
+        """Сохраняем пост в закладках"""
+        bookmarks = self.get_bookmarks()
+        bookmarks.append(post)
+        self.save_bookmarks(bookmarks)
+
+    def delete_bookmarks(self, post_id):
+        """Удаляем пост из закладок"""
+        bookmarks = self.get_bookmarks()
+        for index, book in enumerate(bookmarks):
+            if book['pk'] == post_id:
+                del bookmarks[index]
+                break
+        self.save_bookmarks(bookmarks)
